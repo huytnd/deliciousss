3ee3937231f84c405853e6ff583ac097c6f76c13
Huy Tran <huytnd@hotmail.com>
2022-03-22 17:10:21 +0700

    modified components and pages

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Home from "./Home";
+import Cuisine from "./Cuisine";
+import { Route, Routes } from "react-router-dom";
 
-export default function Pages() {
+function Pages() {
   return (
-    <Home />
-  )
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/cuisine/:type" element={<Cuisine />}></Route>
+    </Routes>
+  );
 }
+
+export default Pages
